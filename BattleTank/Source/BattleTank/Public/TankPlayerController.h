9cd852f945cd304a6d4469a901d02368ce972f13
Martins <kalninsmartins1@gmail.com>
2018-04-06 20:16:26 +0100

    - Added Tank and TankPlayerController classes.

---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+class ATank;
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+	ATank* GetPlayerTank() const;
+	
+	
+};
