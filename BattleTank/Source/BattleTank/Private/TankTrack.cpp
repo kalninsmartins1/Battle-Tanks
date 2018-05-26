e6a8931f8c1cc2351d4bf23e6c399ead266bdfaa
Martins <kalninsmartins1@gmail.com>
2018-05-26 08:48:14 +0100

    - Left track, right track input.

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+ 
+
+
+void UTankTrack::SetTrottle(float value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("TankTrack: Trottle %f"), value);
+}
